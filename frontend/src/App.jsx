82cb94215028374f3e27cd9354d43fd08062936c
Kolkenn <leonel.ponce96@gmail.com>
2025-09-08 16:02:23 -0400

    Version 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="p-4">
+            <h1 className="text-3xl font-bold text-green-600">
+                Welcome to LawnLedgers!
+            </h1>
+            <p className="mt-2">Your environment is set up correctly.</p>
+        </div>
+    )
+}
+export default App
