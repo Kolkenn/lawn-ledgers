c6268fb789e8ff2310d2f01e47e395fbd6dc2c58
Kolkenn <leonel.ponce96@gmail.com>
2025-09-08 13:54:34 -0400

    Initialize a new React + Vite project. Fixes #6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // Use the SWC plugin
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 })
