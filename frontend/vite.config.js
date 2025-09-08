82f9c99db7e06cf888de4f5d4a5a9b3a49ed99d9
Kolkenn <leonel.ponce96@gmail.com>
2025-09-08 16:28:26 -0400

    Clean Install + Vite React Template BASE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc' // Use the SWC plugin
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(), 
-        tailwindcss()
-    ],
+  plugins: [react()],
 })
