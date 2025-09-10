25026bd1127022e2ffbe3e7a34a8fcb120ad11d4
Kolkenn <leonel.ponce96@gmail.com>
2025-09-10 11:48:53 -0400

    Implemented language detection from browser and caching to remember a user's language choice.

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,19 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en/translation.json';
 import es from './locales/es/translation.json';
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+.use(LanguageDetector)  
+.use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
       en: { translation: en },
       es: { translation: es },
     },
-    lng: 'en', // default language
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // react already safes from xss
+    },
+    detection: {
+      order: ['localStorage', 'navigator'], // Check localStorage first, then the browser's language
+      caches: ['localStorage'], // Where to cache the user's choice
     },
   });
 
