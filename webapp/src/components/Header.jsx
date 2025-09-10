8f7659cf41405b564071b7e1b3d2672417271b58
Kolkenn <leonel.ponce96@gmail.com>
2025-09-10 18:01:14 -0400

    Added Logo to Header and also properly provided feedback to user when changing the Logo.

--- webapp/src/components/Header.jsx
+++ webapp/src/components/Header.jsx
@@ -25,10 +25,20 @@ const Header = ({ user, companyProfile }) => {
   return (
     <header className="bg-white border-b border-gray-200 p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-xl font-semibold text-gray-800">
-          {companyProfile?.companyName}
-          {pageTitle && <span className="text-gray-400 font-normal"> - {pageTitle}</span>}
-        </h1>
+        <div className="flex items-center">
+          {/* Conditionally render the logo if the URL exists */}
+          {companyProfile?.logoUrl && (
+            <img 
+              src={companyProfile.logoUrl} 
+              alt={`${companyProfile.companyName} logo`}
+              className="w-8 h-8 rounded-md object-contain mr-3"
+            />
+          )}
+          <h1 className="text-xl font-semibold text-gray-800">
+            {companyProfile?.companyName}
+            {pageTitle && <span className="text-gray-400 font-normal"> - {pageTitle}</span>}
+          </h1>
+        </div>
         <div className="flex items-center space-x-2">
           <LanguageSwitcher />
           <button
